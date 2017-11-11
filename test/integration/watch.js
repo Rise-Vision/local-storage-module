b4c7c7994cd047171babb8be9e0aa516ffc86e2b
Stuart Lees <stuartlees@gmail.com>
2017-11-10 19:51:16 -0500

    Fix watch integration test to delete database when complete

--- test/integration/watch.js
+++ test/integration/watch.js
@@ -1,31 +1,41 @@
 /* eslint-env mocha */
 const commonConfig = require("common-display-module");
 const simple = require("simple-mock");
-const messaging = require("../../src//messaging/messaging");
-const database = require("../../src//db/lokijs/database");
 const os = require("os");
+const messaging = require("../../src/messaging/messaging");
+const database = require("../../src/db/lokijs/database");
 const localMessagingModule = require("local-messaging-module");
+const path = require("path");
+const {platform} = require("rise-common-electron");
+const dbSaveInterval = 5;
 
 global.log = {file: ()=>{}};
 
 describe("WATCH: Integration", function() {
+  const tempDBPath = path.join(os.tmpdir(), "lokijs_test_dir");
+
   describe("Connected to Messaging Service through Local Messaging", ()=>{
     before(()=>{
       simple.mock(commonConfig, "getDisplaySettingsSync").returnWith({
         displayid: "ls-test-id", displayId: "ls-test-id"
       });
 
-      return localMessagingModule.start()
-      .then(()=>database.start(os.tmpdir()))
-      .then(()=>database.destroy())
+      return platform.mkdirRecursively(tempDBPath)
+      .then(()=>localMessagingModule.start())
+      .then(()=>database.start(tempDBPath, dbSaveInterval))
       .then(messaging.init);
     });
 
     after(()=>{
       commonConfig.disconnect();
       localMessagingModule.stop();
-      database.close();
-    })
+
+      // allow for db save interval to complete before destroying
+      return setTimeout(()=>{
+        database.destroy();
+        database.close();
+      }, dbSaveInterval * dbSaveInterval);
+    });
 
     beforeEach(()=>{
       simple.mock(commonConfig, "getMachineId").returnWith("0");
