18649dc6f7aafbba29bcdd4c0407375228bbec92
Stuart Lees <stuartlees@gmail.com>
2017-11-13 17:14:12 -0500

    Refactor test coverage for MS message changes

--- test/unit/messaging/update/entry.js
+++ test/unit/messaging/update/entry.js
@@ -1,36 +1,27 @@
 /* eslint-env mocha */
 const assert = require("assert");
 const simple = require("simple-mock");
+const gcsValidator = require("gcs-filepath-validator");
 const entry = require("../../../../src/messaging/update/entry");
 
 describe("UPDATE entry", ()=> {
 
-  describe("validateAndFilter", ()=> {
+  describe("validate", ()=> {
+    beforeEach(()=>{
+      simple.mock(gcsValidator, "validateFilepath").returnWith(true);
+    });
+
     afterEach(() => {
       simple.restore();
     });
   });
 
-
-  it("should return false if files is not an Array of file objects with correct props", ()=> {
-    assert.equal(entry.validateAndFilter(), false);
-    assert.equal(entry.validateAndFilter([]), false);
-    assert.equal(entry.validateAndFilter([{}]), false);
-    assert.equal(entry.validateAndFilter([{filePath: "test-file-path"}]), false);
-  });
-
-  it("should return an array of filtered file objects", ()=>{
-    const arr1 = [
-      {filePath: "test-file-path1", version: "1.0.0", token: "abc123"},
-      {filePath: "test-file-path1", version: "1.0.0", token: "def456"}
-      ];
-    const arr2 = [
-      {filePath: "test-file-path1"},
-      {filePath: "test-file-path2", version: "1.0.0", token: "abc123"}
-      ];
-
-    assert.deepEqual(entry.validateAndFilter(arr1), arr1);
-    assert.deepEqual(entry.validateAndFilter(arr2), [arr2[1]]);
+  it("should validate correct entry value", ()=> {
+    assert.equal(entry.validate(), false);
+    assert.equal(entry.validate({filePath: "test-file"}), false);
+    assert.equal(entry.validate({filePath: "test-file", version: "test-version"}), false);
+    assert.equal(entry.validate({filePath: "test-file", version: "test-version", token: {}}), false);
+    assert.equal(entry.validate({filePath: "test-file", version: "test-version", token: {hash: "abc123", data: {timestamp: Date.now(), filePath: "test-file", displayId: "test-display"}}}), false);
   });
 
 });
