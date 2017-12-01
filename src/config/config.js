8d2cd1818e5696df3ac679a9f5cae48f0d0e81ae
Stuart Lees <stuartlees@gmail.com>
2017-12-01 11:31:15 -0500

    Improved config use of module name

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,15 @@
+const MODULE_NAME = "local-storage";
+
 let displayId = "";
 let moduleVersion = "";
 
 module.exports = {
   secondMillis: 1000,
-  moduleName: "local-storage",
+  moduleName: MODULE_NAME,
   bqProjectName: "client-side-events",
   bqDatasetName: "Module_Events",
-  bqFailedEntryFile: "local-storage-failed.log",
-  bqTableName: "local-storage-events",
+  bqFailedEntryFile: `${MODULE_NAME}-failed.log`,
+  bqTableName: `${MODULE_NAME}-events`,
   setDisplayId(id) {displayId = id;},
   getDisplayId() {return displayId;},
   setModuleVersion(version) {moduleVersion = version;},
