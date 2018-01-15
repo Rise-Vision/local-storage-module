bca1dc94f4f84b25a068ea458a1c7e5eaecf5cf2
Stuart Lees <stuartlees@gmail.com>
2018-01-15 16:22:42 -0500

    Ensure to broadcast via web socket when "ws-client" is an owner

--- src/messaging/broadcast-ipc.js
+++ src/messaging/broadcast-ipc.js
@@ -1,16 +1,21 @@
 const commonConfig = require("common-display-module");
 const fileSystem = require("../files/file-system");
 const config = require("../config/config");
+const db = require("../db/api");
 
 module.exports = {
   broadcast(topic, data = {}) {
-    commonConfig.broadcastMessage(Object.assign(
-      {
-        from: config.moduleName,
-        topic
-      },
-      data
-    ));
+    const message = Object.assign({from: config.moduleName, topic}, data);
+    const owners = db.owners.get(data.filePath);
+
+    // ensure to broadcast via websocket if "ws-client" is an owner
+    if (owners && owners.includes("ws-client")) {
+      commonConfig.broadcastToLocalWS(message);
+
+      if (owners.length === 1) {return;}
+    }
+
+    commonConfig.broadcastMessage(message);
   },
   fileUpdate(data = {}) {
     log.file(`Broadcasting ${data.status} FILE-UPDATE for ${data.filePath}`);
--- test/unit/messaging/broadcast-ipc.js
+++ test/unit/messaging/broadcast-ipc.js
@@ -2,12 +2,17 @@
 const assert = require("assert");
 const broadcast = require("../../../src/messaging/broadcast-ipc");
 const fileSystem = require("../../../src/files/file-system");
+const db = require("../../../src/db/api");
+const commonConfig = require("common-display-module");
+const config = require("../../../src/config/config");
 const simple = require("simple-mock");
 
 describe("Broadcast IPC", ()=> {
   beforeEach(()=>{
     simple.mock(fileSystem, "getPathInCache").returnWith("fake-os-path");
-    simple.mock(broadcast, "broadcast").returnWith();
+    simple.mock(commonConfig, "broadcastMessage").returnWith();
+    simple.mock(commonConfig, "broadcastToLocalWS").returnWith();
+    simple.mock(db.owners, "get").returnWith([]);
   });
 
   afterEach(()=>{
@@ -16,17 +21,39 @@ describe("Broadcast IPC", ()=> {
 
   it("broadcasts file update", ()=>{
     broadcast.fileUpdate({filePath: "test-file-path", status: "test-status"});
-    assert.equal(broadcast.broadcast.callCount, 1);
-    assert.equal(broadcast.broadcast.lastCall.args[0], "FILE-UPDATE");
-    assert.equal(broadcast.broadcast.lastCall.args[1].ospath, "fake-os-path");
-    assert.equal(broadcast.broadcast.lastCall.args[1].filePath, "test-file-path");
-    assert.equal(broadcast.broadcast.lastCall.args[1].status, "test-status");
+    assert.equal(commonConfig.broadcastMessage.callCount, 1);
+    assert.deepEqual(commonConfig.broadcastMessage.lastCall.args[0], {
+      from: config.moduleName,
+      topic: "FILE-UPDATE",
+      filePath: "test-file-path",
+      ospath: "fake-os-path",
+      status: "test-status"
+    });
+  });
+
+  it("broadcasts file update to websocket client", ()=>{
+    simple.mock(db.owners, "get").returnWith(["ws-client"]);
+
+    broadcast.fileUpdate({filePath: "test-file-path", status: "test-status"});
+    assert.equal(commonConfig.broadcastToLocalWS.callCount, 1);
+    assert.equal(commonConfig.broadcastMessage.callCount, 0);
+    assert.deepEqual(commonConfig.broadcastToLocalWS.lastCall.args[0], {
+      from: config.moduleName,
+      topic: "FILE-UPDATE",
+      filePath: "test-file-path",
+      ospath: "fake-os-path",
+      status: "test-status"
+    });
   });
 
   it("broadcasts file error", ()=>{
     broadcast.fileError({filePath: "test-file-path", msg: "test-message"});
-    assert.equal(broadcast.broadcast.callCount, 1);
-    assert.equal(broadcast.broadcast.lastCall.args[0], "FILE-ERROR");
-    assert.equal(broadcast.broadcast.lastCall.args[1].filePath, "test-file-path");
+    assert.equal(commonConfig.broadcastMessage.callCount, 1);
+    assert.deepEqual(commonConfig.broadcastMessage.lastCall.args[0], {
+      from: config.moduleName,
+      topic: "FILE-ERROR",
+      filePath: "test-file-path",
+      msg: "test-message"
+    });
   });
 });
