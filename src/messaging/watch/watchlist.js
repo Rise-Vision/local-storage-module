40cc59c49657a4bf7b451ec29411da0e5734c6f9
santiagonoguez <santiago@nul-unu.com>
2018-03-19 10:29:03 -0600

    refresh updated files

--- src/messaging/watch/watchlist.js
+++ src/messaging/watch/watchlist.js
@@ -1,8 +1,30 @@
 const commonMessaging = require("common-display-module/messaging");
 const db = require("../../db/api");
+const watch = require("./watch");
 
 function updateFilesStatusAndRequestUpdatedFiles(filePaths) {
-  return Promise.resolve(filePaths);
+  if (filePaths.length === 0) {
+    return Promise.resolve();
+  }
+
+  log.all('refreshing files', filePaths.join(', '));
+
+  return Promise.all(filePaths.map(filePath => {
+    const metaData = db.fileMetadata.get(filePath);
+    const item = db.owners.get(filePath);
+
+    // will happen when folders contain new entries, but we'll ignore those for now
+    if (!item || !metaData) {
+      return false;
+    }
+
+    // just take the first, so process() call won't fail
+    const from = item.owners[0];
+    metaData.status = "UNKNOWN";
+
+    return db.fileMetadata.put(metaData)
+    .then(() => watch.process({filePath, from}));
+  }));
 }
 
 function requestWatchlistCompare() {
