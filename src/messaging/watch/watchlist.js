07bd82d7d1d73704df24eb2b1da6ae1674aa7dba
santiagonoguez <santiago@nul-unu.com>
2018-03-20 15:13:20 -0600

    add watchlist compare hander and file update

--- src/messaging/watch/watchlist.js
+++ src/messaging/watch/watchlist.js
@@ -1,5 +1,6 @@
 const commonMessaging = require("common-display-module/messaging");
 const db = require("../../db/api");
+const update = require("../update/update");
 
 function requestWatchlistCompare() {
   const lastChanged = db.watchlist.lastChanged();
@@ -8,6 +9,34 @@ function requestWatchlistCompare() {
   commonMessaging.sendToMessagingService(msMessage);
 }
 
+function addOrUpdate(filePath, version) {
+  const previousEntry = db.fileMetadata.get(filePath);
+  const updatedEntry = Object.assign({}, previousEntry, {filePath, version});
+
+  return update.update(updatedEntry);
+}
+
+function setUnknownStatusToMissingFiles(watchlist) {
+  console.log(watchlist);
+}
+
+function refresh(watchlist, lastChanged) {
+  const filePaths = Object.keys(watchlist);
+
+  if (watchlist.length === 0) {
+    return Promise.resolve();
+  }
+
+  return Promise.all(filePaths.map(filePath => {
+    const version = watchlist[filePath];
+
+    return addOrUpdate(filePath, version);
+  }))
+  .then(() => setUnknownStatusToMissingFiles(watchlist))
+  .then(() => db.watchlist.setLastChanged(lastChanged));
+}
+
 module.exports = {
+  refresh,
   requestWatchlistCompare
 };
