d29359517c50c8959050fae1cbbe329ff8e85351
santiagonoguez <santiago@nul-unu.com>
2018-03-27 14:21:37 -0600

    change lastChanged test values to string

--- test/integration/messaging/messaging.js
+++ test/integration/messaging/messaging.js
@@ -111,7 +111,7 @@ describe("WATCH: Integration", function() {
         topic: "msfileupdate",
         type: "update",
         filePath,
-        watchlistLastChanged: 123456,
+        watchlistLastChanged: "123456",
         version: "test-version-updated",
         token
       });
@@ -122,7 +122,7 @@ describe("WATCH: Integration", function() {
         assert.equal(api.fileMetadata.get(filePath).version, "test-version-updated");
         assert.equal(api.fileMetadata.get(filePath).status, "STALE");
         assert.deepEqual(api.fileMetadata.get(filePath).token, token);
-        assert.equal(api.watchlist.lastChanged(), 123456);
+        assert.equal(api.watchlist.lastChanged(), "123456");
 
         assert.equal(api.watchlist.get(filePath).version, "test-version-updated");
         done();
@@ -139,7 +139,7 @@ describe("WATCH: Integration", function() {
       commonMessaging.broadcastMessage({
         topic: "msfileupdate",
         type: "delete",
-        watchlistLastChanged: 123458,
+        watchlistLastChanged: "123458",
         filePath
       });
 
@@ -150,7 +150,7 @@ describe("WATCH: Integration", function() {
               assert.equal(message.status, "DELETED");
               assert(!api.fileMetadata.get(filePath));
               assert(!api.watchlist.get(filePath));
-              assert.equal(api.watchlist.lastChanged(), 123458);
+              assert.equal(api.watchlist.lastChanged(), "123458");
 
               res();
             }
