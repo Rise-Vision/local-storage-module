73595b188338098bc642ddc3a58ec65bfd19b954
santiagonoguez <santiago@nul-unu.com>
2018-06-21 12:56:46 -0500

    add tests for expired folder

--- test/integration/expiration.js
+++ test/integration/expiration.js
@@ -48,7 +48,7 @@ describe("expiration - integration", () => {
     const testEntries = [
       {filePath: "a.txt", status: "STALE", version: "1"},
       {filePath: "b.txt", status: "CURRENT", watchSequence: 1},
-      {filePath: "c.txt", status: "CURRENT", watchSequence: 1}
+      {filePath: "folder/", watchSequence: 1}
     ];
 
     db.fileMetadata.put(testEntries[0]);
@@ -78,6 +78,37 @@ describe("expiration - integration", () => {
     db.watchlist.put(testEntries[0]);
     db.watchlist.put(testEntries[1]);
     db.watchlist.put(testEntries[2]);
+
+    Array(5).fill().forEach(db.watchlist.increaseRuntimeSequence);
+
+    return expiration.cleanExpired()
+    .then(() => {
+      const metadataEntries = db.fileMetadata.allEntries();
+      assert.equal(metadataEntries.length, 1);
+      assert.equal(metadataEntries[0].filePath, "a.txt");
+
+      const watchlistEntries = db.watchlist.allEntries();
+      assert.equal(watchlistEntries.length, 1);
+      assert.equal(watchlistEntries[0].filePath, "a.txt");
+    });
+  });
+
+  it("expires folder entries if their watchSequence is too old", ()=>{
+    const testEntries = [
+      {filePath: "a.txt", status: "STALE", version: "1"},
+      {filePath: "folder/", watchSequence: 1},
+      {filePath: "folder/b.txt", status: "CURRENT"},
+      {filePath: "folder/c.txt", status: "CURRENT"}
+    ];
+
+    db.fileMetadata.put(testEntries[0]);
+    db.fileMetadata.put(testEntries[1]);
+    db.fileMetadata.put(testEntries[2]);
+    db.fileMetadata.put(testEntries[3]);
+    db.watchlist.put(testEntries[0]);
+    db.watchlist.put(testEntries[1]);
+    db.watchlist.put(testEntries[2]);
+    db.watchlist.put(testEntries[3]);
 
     Array(5).fill().forEach(db.watchlist.increaseRuntimeSequence);
 
