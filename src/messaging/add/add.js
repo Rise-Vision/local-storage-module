0ff030231c9fd3cce02532abcbf1ca7d16439028
Tyler Johnson <mail@tyler-johnson.ca>
2018-08-09 13:29:56 -0400

    Update owners when overwriting trashed file
    When a trashed file is overwritten, an update is received, not an ADD.
    
    In this case we need to make sure the same folder watcher check is
    performed to add those watchers as owners of the file. If there are no
    watchers, no harm done, we continue the update as before.
    
    The differernce with ADD is that if there are no folder watchers,
    there's nothing else to do and no update on the metadata is performed.

--- src/messaging/add/add.js
+++ src/messaging/add/add.js
@@ -1,27 +1,9 @@
-const {dirname} = require("path");
-const db = require("../../db/api");
 const update = require("../update/update");
-const logger = require("../../logger");
 
 module.exports = {
-  assignOwnersOfParentDirectory(message, topic) {
-    const {filePath} = message;
-
-    const folderPath = `${dirname(filePath)}/`;
-    const folderItem = db.owners.get(folderPath);
-
-    if (!folderItem) {
-      logger.warning(`No owners registered for folder ${folderPath} | topic: ${topic}`);
-
-      return Promise.resolve(false);
-    }
-
-    db.owners.put({filePath, owners: folderItem.owners});
-    return Promise.resolve(true);
-  },
   process(message) {
     return update.validate(message, "add")
-    .then(() => module.exports.assignOwnersOfParentDirectory(message, 'MSFILEUPDATE'))
+    .then(()=>update.assignOwnersOfParentDirectory(message, 'MSFILEUPDATE'))
     .then(assigned => assigned && update.update(message));
   }
 };
